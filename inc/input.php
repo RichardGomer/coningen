937ff8f498e4194c541ef4902be9028d60a38c2b
Richard Gomer <rcg1v07@soton.ac.uk>
2017-03-07 19:27:07 +0000

    Add scoring, statement-signal indication, instructions

--- inc/input.php
+++ inc/input.php
@@ -4,7 +4,8 @@
     <label>Service/Interaction Name</label>
     <input type="text" name="title" id="title" />
 
-    <label>Enter purpose statements, one per line</label>
+    <label>Enter purpose statements, one per line. Optionally, provide consent signal IDs in brackets first, separated by commas<br />
+    eg: &nbsp; <tt>(A,D) send you emails about new products that you might be interested in</tt></label>
     <textarea name="st" id="st"></textarea>
 
     <label>Statement introduction phrase</label>
@@ -13,9 +14,48 @@
     <label>Include extended meta-section (for printing interview protocol etc.)</label>
     <input type="checkbox" value="1" name="extmeta" id="extmeta" checked="checked" />
 
-    <label>Include standard rubrics</label>
+    <label>Include standard rubrics and instructions</label>
     <input type="checkbox" value="1" name="rubric" id="rubric" checked="checked" />
 
     <hr />
     <input type="submit" value="Generate" />
 </form>
+
+<h2>Instructions</h2>
+
+<p>This tool generates a consentfulness scoring instrument that can be used to assess the consentfulness of a given service-interaction
+pair.  By service, we mean a set of data processes that are contingent on <em>consent signals</em> that received from the data subject
+via a <em>consent interaction</em>.  A consent interaction could be, for instance, an online form, or a face-to-face conversation.</p>
+
+<p>Each process is described by one or more <em>purpose statements</em>.  These statements should (taken together) describe  all the reasons
+that the data is being processed, and any data derived from that processing.  For instance "use data about your location to identify your
+home city" or "use data about your location to choose which advertisements you are shown."</p>
+
+<p>These statements should be entered into the form above, along with the ID of the consent signal that they relate to.  For instance, if
+signal 'A' nominally gives consent to email marketing, enter "(A) send you emails about relevant offers from our company".  If a statement
+applies to multiple signals (for instance there are multiple consent signals that nominally give consent to a particular process) then enter
+them separated by commas, like so "(A,C) purpose statement."</p>
+
+<p>This generator provides a survey instrument that can be administered to participants in a study, plus a scoring table to help
+    calculate the individual consentfulness score for that participant.  As exaplined in the notes below, you'll need to repeat the instrument
+    with multiple participants to get an overall score.</p>
+
+
+<h3>Notes on consentfulness</h3>
+
+<p>Consentfulness is, notionally, the proportion of consent <em>signals</em> that are voluntary and do not lead to unexpected actions.</p>
+
+<p>Consentfulness is a tool for population-level analysis of a particular service-interaction combination.  It doesn't make sense to apply
+    it to a single person's interactions.  The consentfulness scores from each individual application of the generated instrument should
+be averaged across the target population to obtain an estimate of actual consetnfulness.</p>
+
+<p>Consentfulness is population and context specific.  Many important aspects of consent - such as attention, comprehension, and capability -
+    are highly dependent on the <em>person</em> who is being asked to give consent, and the <em>context</em> in which they are being asked.  Exercise
+    caution when generalising consentfulness scores, and report them along with information about the population and context that was examined.</p>
+
+<h2>About ConInGen</h2>
+
+<p>The Consentfulness Instrument Generator is developed by the <strong><a href="http://www.meaningfulconsent.org/">Meaningful Consent in the Digital Economy
+    Project</a></strong> at the University of Southampton.</p>
+
+<p>Consentfulness is a work in progress, please don't rely on it in court.  It might not work at all.</p>
--- inc/instrument.php
+++ inc/instrument.php
@@ -18,6 +18,8 @@ if(array_key_exists('extmeta', $_POST))
     ));
 }
 
+$rubric = array_key_exists('rubric', $_POST);
+
 ?>
 
 <table class="meta">
@@ -30,7 +32,7 @@ if(array_key_exists('extmeta', $_POST))
 
 <?php
 
-if(array_key_exists('rubric', $_POST))
+if($rubric)
 {
     ?>
 
@@ -57,19 +59,48 @@ if(array_key_exists('rubric', $_POST))
 
 $c = 1;
 
-$st = explode("\n", $_POST['st']);
+$rawstatements = explode("\n", $_POST['st']);
 $intro = $_POST['intro'];
-foreach($st as $statement)
+
+$signals = array();
+
+foreach($rawstatements as $st)
 {
-    if(strlen(trim($statement)) < 1)
+    if(strlen(trim($st)) < 1)
         continue;
 
     if(strlen($intro) > 0)
-        $statement = lcfirst($statement);
+        $st = lcfirst($st);
 
+    // Get the signal identifier, if there is one
+    if(preg_match('/^\((.+)\)(.+)/', $st, $matches))
+    {
+        $sigs = explode(',', $matches[1]);
+        $st = trim($matches[2]);
+    }
+    else
+    {
+        $sigs = array(false);
+    }
+
+    // Store the statement itself
+    $statements[$c] = $st;
+
+    // Store the statement ID against the signals it relates to
+    foreach($sigs as $sig)
+    {
+        if(!array_key_exists($sig, $signals))
+        {
+            $signals[$sig] = array();
+        }
+
+        $signals[$sig][] = $c;
+    }
+
+    // Print the instrument atom
     echo <<<END
     <table class="instrumentatom">
-    <tr class="statement"><td colspan="3"><span class="num">$c</span> $intro $statement</td></tr>
+    <tr class="statement"><td colspan="3"><span class="num">$c</span> $intro $st</td></tr>
     <tr><td></td><td class="expect">I expected this</td><td class="notexpect">I did <em>not</em> expect this</td></tr>
     <tr><td class="happy">I am happy for this to take place</td><td></td><td></td></tr>
     <tr><td class="nothappy">I am <em>not</em> happy for this to take place</td><td></td><td></td></tr>
@@ -79,5 +110,71 @@ END;
     $c++;
 }
 
+/**
+ * Show scoring chart
+ */
 ?>
+<h2>Scoring</h2>
+
+<?php if($rubric) { ?>
+<p>Each atom is scored on two criteria: Expectation, based on the columns; and Voluntariness,
+based on the rows.  It may pass or fail either, independently.</p>
+<p>Use this table to score the overall instrument.  If a statement is failed (it was unexpected, or
+involuntary) mark all boxes on that row. You may score for expectation, voluntariness or both.  If
+scoring for both, mark statements that fail on <em>either</em> of the criteria.</p>
+
+<p>In the failure row, mark the boxes if there are ANY failures in that column.</p>
+
+<p>Let failed_signals = the number of failures in the last row</p>
+<math display="block">
+    <mi>consentfulness</mi> <mo>=</mo>
+    <mn>1</mn> <mo>-</mo>
+    <mfrac>
+        <mi>failed_signals</mi>
+        <mn><?php echo count($signals); ?></mn>
+    </mfrac>
+</math>
+<?php }
+
+echo '
+<table class="score">
+<tr><td></td>';
+foreach($signals as $sig=>$stids)
+{
+    echo "<td>$sig</td>";
+}
+echo "</tr>\n";
+
+foreach($statements as $sid=>$st)
+{
+    echo "<tr><td>$sid: $st</td>";
+
+    // Show a scoring box against each relevant signal
+    foreach($signals as $sig=>$stids)
+    {
+        if(in_array($sid, $stids))
+        {
+            echo "<td class=\"scorebox\"></td>";
+        }
+        else
+        {
+            echo "<td class=\"ignorebox\"></td>";
+        }
+    }
+
+    echo "</tr>\n";
+}
+
+echo '<tr><td>FAIL</td>';
+foreach($signals as $sig=>$stids)
+{
+    echo "<td class=\"totbox\"></td>";
+}
+echo "</tr>\n</table>\n";
+
+?>
+
+<div id="results">
+</div>
+
 </div>
